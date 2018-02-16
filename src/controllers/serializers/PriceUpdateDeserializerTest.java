c2d9d6852c07d68b868af7cf5de2163aef5b6105
raghavendra.udyavar <raghavs.mail@gmail.com>
2018-02-16 11:58:05 +0800

    JUnit test framework added

---
+++ src/controllers/serializers/PriceUpdateDeserializerTest.java
@@ -0,0 +1,16 @@
+package controllers.serializers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceUpdateDeserializerTest {
+
+    @Test
+    void deserialize() {
+    }
+
+    @Test
+    void isPriceUpdateInputNullOrEmpty() {
+    }
+}
