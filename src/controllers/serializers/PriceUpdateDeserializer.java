cbc17a503193051aae3a774f2d02025654b48c65
raghavendra.udyavar <raghavs.mail@gmail.com>
2018-02-16 13:32:41 +0800

    getTotalPriceUpdates and getIndividualPriceUpdates added and tested

--- src/controllers/serializers/PriceUpdateDeserializer.java
+++ src/controllers/serializers/PriceUpdateDeserializer.java
@@ -8,25 +8,45 @@ import data.PriceUpdate;
 // <TimeStamp> <Exchange> <Source currency> <Destination Currency> <Forward factor> <Backward Factor>
 public class PriceUpdateDeserializer implements IDeserializer {
 
-    public PriceUpdate deserialize(String priceUpdateInput) throws Exception{
+    public PriceUpdate deserialize(String input) throws Exception{
 
         System.out.println("Deserialization");
 
         // check if the priceupdate is empty
-        if(!isPriceUpdateInputNullOrEmpty(priceUpdateInput)) {
+        if(isInputInRightFormat(input)) {
             // check if the format is right
 
         }
         return  null;
     }
 
-    boolean isPriceUpdateInputNullOrEmpty(String priceUpdateInput) throws EmptyPriceUpdateException{
-        if(priceUpdateInput == null){
+    boolean isInputNullOrEmpty(String input) throws EmptyPriceUpdateException {
+        if(input == null){
             throw new EmptyPriceUpdateException("PriceUpdateInput cannot be null");
-        } else if(priceUpdateInput.isEmpty()){
+        } else if(input.isEmpty()){
             throw new EmptyPriceUpdateException("PriceUpdateInput cannot be empty");
         } else {
             return false;
         }
+    }
+
+    boolean isInputInRightFormat(String input) throws EmptyPriceUpdateException {
+        if(!isInputNullOrEmpty(input)){
+
+        }
+        return false;
+    }
+
+    String[] getTotalPriceUpdates(String input){
+
+        // multiple priceupdates are separated by a newline character
+        return input.split("\n");
+    }
+
+    String[] getIndividualPriceUpdate(String input){
+
+        // separate the priceupdate from the input string
+        String[] priceUpdateStrings = input.split(" ");
+        return priceUpdateStrings;
     }
 }
--- src/controllers/serializers/PriceUpdateDeserializerTest.java
+++ src/controllers/serializers/PriceUpdateDeserializerTest.java
@@ -9,6 +9,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PriceUpdateDeserializerTest {
 
+    PriceUpdateDeserializer priceUpdateDeserializer = new PriceUpdateDeserializer();
+
+    String newPriceUpdateString = "2017-11-01T09:42:23+00:00 KRAKEN BTC USD 1000.0 0.0009\n"
+            + "2017-11-01T09:42:23+00:00 GDAX ETH SGD 1001.0 0.0008\n"
+            + "2017-11-01T09:42:23+00:00 GEMINI BTC USD 1002.0 0.0005\n"
+            + "2017-11-01T09:42:23+00:00 BITCXINDIA ALT INR 1003.0 0.0005\n"
+            + "2017-11-01T09:42:23+00:00 COINCHECK BTC USD 1004.0 0.0005";
+
     @Test
     void deserialize() {
     }
@@ -17,8 +25,7 @@ class PriceUpdateDeserializerTest {
     void isPriceUpdateInputNull() {
 
         try {
-            PriceUpdateDeserializer priceUpdateDeserializer = new PriceUpdateDeserializer();
-            priceUpdateDeserializer.isPriceUpdateInputNullOrEmpty(null);
+            priceUpdateDeserializer.isInputNullOrEmpty(null);
         } catch (Exception ex) {
             assertThrows(EmptyPriceUpdateException.class, () -> { throw new EmptyPriceUpdateException("PriceUpdateInput cannot be null"); }, "price update is null");
         }
@@ -26,11 +33,25 @@ class PriceUpdateDeserializerTest {
 
     @Test
     void isPriceUpdateInputEmpty(){
+
         try {
-            PriceUpdateDeserializer priceUpdateDeserializer = new PriceUpdateDeserializer();
-            priceUpdateDeserializer.isPriceUpdateInputNullOrEmpty("");
+            priceUpdateDeserializer.isInputNullOrEmpty("");
         } catch (Exception ex) {
-            assertThrows(EmptyPriceUpdateException.class, () -> { throw new EmptyPriceUpdateException("PriceUpdateInput cannot be null"); }, "price update is null");
+            assertThrows(EmptyPriceUpdateException.class, () -> { throw new EmptyPriceUpdateException("PriceUpdateInput cannot be empty"); }, "price update is null");
         }
+    }
+
+    @Test
+    void CheckPriceUpdateCount(){
+
+        String[] totalPriceUpdates = priceUpdateDeserializer.getTotalPriceUpdates(newPriceUpdateString);
+        assertEquals(5, totalPriceUpdates.length);
+    }
+
+    @Test
+    void CheckIndividualPriceUpdate() {
+        String[] totalPriceUpdates = priceUpdateDeserializer.getTotalPriceUpdates(newPriceUpdateString);
+        String[] firstPriceUpdateValues = priceUpdateDeserializer.getIndividualPriceUpdate(totalPriceUpdates[0]);
+        assertEquals(6, firstPriceUpdateValues.length);
     }
 }
