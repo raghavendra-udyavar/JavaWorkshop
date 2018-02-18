a1054d8cb6f258b7b7f150f8dcd592331e2e4118
raghavendra.udyavar <raghavs.mail@gmail.com>
2018-02-19 00:05:30 +0800

    exhange rate request integration test added

--- src/ExchangeRate.java
+++ src/ExchangeRate.java
@@ -1,4 +1,5 @@
 import controllers.calculator.ExchangeRateCalculator;
+import handlers.ExchangeRequestHandler;
 import handlers.IRequestHandler;
 import handlers.PriceUpdateHandler;
 import models.tables.EdgeWeightedDigraph;
@@ -6,7 +7,7 @@ import models.tables.EdgeWeightedDigraph;
 class ExchangeRate {
     public static void main(String[] args) {
 
-        String newPriceUpdateString = "2017-11-01T09:42:23+00:00 DBS SGD USD 1.00 0.76\n"
+        String priceUpdateInput = "2017-11-01T09:42:23+00:00 DBS SGD USD 1.00 0.76\n"
                 + "2017-11-01T09:42:23+00:00 OCBC SGD RM 1.00 2.97\n"
                 + "2017-11-01T09:42:23+00:00 CITI USD INR 1 64.39\n"
                 + "2017-11-01T09:42:23+00:00 SBI INR SGD 1.00 0.020\n"
@@ -17,8 +18,14 @@ class ExchangeRate {
             ExchangeRateCalculator exchangeRateCalculator = new ExchangeRateCalculator(edgeWeightedDigraph);
 
             IRequestHandler handler = new PriceUpdateHandler(edgeWeightedDigraph, exchangeRateCalculator);
-            handler.HandleRequest(newPriceUpdateString);
+            handler.HandleRequest(priceUpdateInput);
+
+            String exchangeRequestInput =  "EXCHANGE_RATE_REQUEST DBS SGD SBI USD";
+            ExchangeRequestHandler exchangeRequestHandler = new ExchangeRequestHandler(edgeWeightedDigraph, exchangeRateCalculator);
+            exchangeRequestHandler.HandleRequest(exchangeRequestInput);
+
         }catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 }
